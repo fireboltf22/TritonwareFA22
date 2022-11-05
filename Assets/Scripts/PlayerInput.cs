a7cf72a30f187bd209fc8db33de352f7db36d0a6
tcl002 <102575236+tcl002@users.noreply.github.com>
2022-11-04 17:38:55 -0700

    complete pause menu functionality

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,12 +6,11 @@ using UnityEngine.UI;
 public class PlayerInput : MonoBehaviour
 {
 
-    private string input;
+    private string input = "";
     public InputField clear;
-    public Canvas menu;
+    public GameObject pauseMenu;
     private string[] spells = {"fire", "ice", "water", "rock", "shock"};
     private bool correctSpell = false;
-    public bool pause = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,21 +22,23 @@ public class PlayerInput : MonoBehaviour
     void Update()
     {   
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            Debug.Log("Escape key was pressed.");
-            pause = true;
+            TogglePauseMenu();
         }
-        for(int i=0; i<spells.Length; i++){
-            if (string.Equals(input, spells[i])) {
-                Debug.Log("You casted " + spells[i] + ".");
-                correctSpell = true;
+        if(Time.timeScale == 1) {
+            for(int i=0; i<spells.Length; i++){
+                if (string.Equals(input, spells[i])) {
+                    Debug.Log("You casted " + spells[i] + ".");
+                    correctSpell = true;
+                }
             }
+            if (!(string.Equals(input, "")) && (correctSpell == false)) {
+                Debug.Log("You messed up your spell and turned into a frog.");
+            }
+            input = "";
+            correctSpell = false;
+            clear.ActivateInputField();
         }
-        if (!(string.Equals(input, "")) && (correctSpell == false)) {
-            Debug.Log("You messed up your spell and turned into a frog.");
-        }
-        input = "";
-        correctSpell = false;
-        clear.ActivateInputField();
+        
     }
 
     public void ReadStringInput(string s)
@@ -45,6 +46,23 @@ public class PlayerInput : MonoBehaviour
         input = s;
         Debug.Log(input);
         clear.text = "";
+    }
+
+    void OnGUI() {
+        if (Time.timeScale == 0){
+            pauseMenu.SetActive(true);
+        } else {
+            pauseMenu.SetActive(false);
+        }
+    }
+
+    public void TogglePauseMenu() {
+        Debug.Log("Escape key was pressed.");
+        if (Time.timeScale == 0) {
+            Time.timeScale = 1;
+        } else {
+            Time.timeScale = 0;
+        }
     }
 
 }
